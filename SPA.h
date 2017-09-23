dfb7e09878ec79126c9ed628fe462cacff2a0693
Alan <403466681@qq.com>
2017-09-23 16:44:35 +0800

    Update SPA.h

--- SPA.h
+++ SPA.h
@@ -10,8 +10,10 @@ extern int DEBUG_INS[3];	 //上位机指令数组:DEBUG_INS[0]选择操作的参
 				 //		  DEBUG_INS[2]上位机参数低8位
 extern unsigned int S_RECI_EVENT;//串口接收事件标志位
 extern float P_ctrl[3],I_ctrl[3];//控制变量（可定制）
+extern unsigned int S_SEND_CNT;
 
 extern void Event_Process(void);//事件处理机，放主函数内循环调用即可
 extern void Return_Para(void);//参数返回函数，用于验证参数是否返回成功，或读取当前参数
+extern void Send_Matlab(int mode,int *ch);		//matlab示波器接口函数,ch为9个元素的数组
 
 #endif
